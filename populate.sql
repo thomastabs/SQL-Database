72e85652a19a15dc3f0fea6d892b1551bb8e2922
thomastabs <tomassantostaborda@gmail.com>
2023-05-23 21:00:14 +0100

    inicio do populate

--- populate.sql
+++ populate.sql
@@ -1,1 +1,6 @@
+insert into customer values (1, 'Adams', 'adams12345@gmail.com',	'Avenida das Bases de Dados',	918138586);
+insert into customer values (2, 'John', 'john12345@gmail.com',	'Avenida da Interação',	918134543);
+insert into customer values (3, 'Bob', 'bob12345@gmail.com',	'Avenida das Probabilidades', 923423452);
+insert into customer values (4, 'Steve', 'steve12345@gmail.com',	'Avenida dos Sistemas e Sinais', 91712341);
 
+-- (...) --
