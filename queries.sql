597250e1cf1cfd3b32eff6e61b15d1040d48c220
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2023-05-25 20:55:22 +0100

    queries.sql terminado (acho...)

--- queries.sql
+++ queries.sql
@@ -1,38 +1,36 @@
 ----- EXERCICIO 1 -----
-SELECT name
+SELECT customer.name
 FROM customer
-    NATURAL JOIN order
-    NATURAL JOIN contains
+    NATURAL JOIN orders
+    NATURAL JOIN containss
     FULL OUTER JOIN product ON product.name = customer.name
 WHERE date > '2022-12-31' AND date < '2024-01-01' AND price > 50;
 
 ----- EXERCICIO 2 -----
 
-SELECT name
+SELECT employee.name
 FROM employee
     NATURAL JOIN (
-        SELECT order_date
-        FROM order
-        WHERE date > '2022-31-12' AND date < '2023-02-01'
-    )
+        SELECT date
+        FROM orders
+        WHERE date > '2022-31-12' AND date < '2023-02-01') AS january_requests
     NATURAL JOIN process
     FULL OUTER JOIN works ON works.name = employee.name
-    NATURAL JOIN warehouse WHERE warehouse_workplace_address NOT IN(
-        SELECT office_workplace_address FROM office
-    );
+    NATURAL JOIN warehouse WHERE address NOT IN(
+        SELECT office.address FROM office);
 
 
 
 ----- EXERCICIO 3 -----
 SELECT name
-FROM sale NATURAL JOIN contains NATURAL JOIN product
-HAVING SUM(units) >= ALL (
-    SELECT SUM(units)
-    FROM contains
+FROM sale NATURAL JOIN containss NATURAL JOIN product
+HAVING SUM(qty) >= ALL (
+    SELECT SUM(qty)
+    FROM containss
 );
 
 ----- EXERCICIO 4 -----
-SELECT order_number, SUM(price * quantity) AS total_value
-FROM sale NATURAL JOIN contains NATURAL JOIN product
-GROUP BY order_number;
+SELECT order_num, SUM(product.price * containss.qty) AS total_value
+FROM sale NATURAL JOIN containss NATURAL JOIN product
+GROUP BY order_num;
 
