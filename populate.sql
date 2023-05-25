d6d2eb1dc7418e85b00df56ecb87f942b7475099
zegallego <themadracer1@gmail.com>
2023-05-25 15:47:17 +0100

    populate

--- populate.sql
+++ populate.sql
@@ -1,6 +1,21 @@
 insert into customer values (1, 'Adams', 'adams12345@gmail.com',	'Avenida das Bases de Dados',	918138586);
-insert into customer values (2, 'John', 'john12345@gmail.com',	'Avenida da Interação',	918134543);
+insert into customer values (2, 'John', 'johnxina@gmail.com',	'Avenida da Interação',	918134543);
 insert into customer values (3, 'Bob', 'bob12345@gmail.com',	'Avenida das Probabilidades', 923423452);
 insert into customer values (4, 'Steve', 'steve12345@gmail.com',	'Avenida dos Sistemas e Sinais', 91712341);
+
+insert into orders values (1, 2023-01-04, 1);
+insert into orders values (2, 2023-01-10, 2);
+insert into orders values (3, 2023-01-17, 1);
+insert into orders values (4, 2023-01-23, 3);
+
+insert into sale values (1);
+insert into sale values (2);
+insert into sale values (3);
+insert into sale values (4);
+
+insert into product values (10000001, 'Vassoura', 'Vassoura de qualidade industrial', 4.20);
+insert into product values (10000002, 'Detergente', 'Detergente multi usos, tão bom que o Fairy até chora', 9.60);
+insert into product values (10000003, 'Walkman', 'Sony Walkman, dispositivo de audição de música em cassete de alta qualidade', 300.00);
+insert into product values (10000004, 'Batatas', 'Saco de batatas irlandesas recem colhidas', 6.90);
 
 -- (...) --
