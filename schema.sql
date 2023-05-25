b5d287ca350604f0eb402297d6d8559260fdcabe
thomastabs <tomassantostaborda@gmail.com>
2023-05-25 18:40:54 +0100

    ahhhhhh

--- schema.sql
+++ schema.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS delivery CASCADE;
 --- (IC-1) Customers can only pay for the Sale of an Order they have placed themselves 
 ------------------------------------------------------------------------------------------------------------------------------------
 
-create table customer
+CREATE TABLE customer
    (number INTEGER NOT NULL,
     name VARCHAR(80) NOT NULL,
     email VARCHAR(255) NOT NULL,
@@ -35,7 +35,7 @@ create table customer
     PRIMARY KEY(number));
 
 -- Order table name is called orders because of the keyword order --
-create table orders
+CREATE TABLE orders
    (order_num INTEGER NOT NULL,
     date DATE NOT NULL,
     customer_number INTEGER NOT NULL,
@@ -43,7 +43,7 @@ create table orders
     FOREIGN KEY(customer_number) REFERENCES customer(number));
     -- IC-1: Every order (order num) must participate in the contains association --
 
-create table product
+CREATE TABLE product
    (sku VARCHAR(8) NOT NULL,
     name VARCHAR(80) NOT NULL,
     description VARCHAR(255) NOT NULL,
@@ -54,7 +54,7 @@ create table product
     -- IC-2: Every product (sku) must participate in the supply entity --
 
 -- Contains table name is called containss with 2 s's because of the keyword contain --
-create table containss
+CREATE TABLE containss
    (order_num INTEGER NOT NULL,
     sku VARCHAR(8) NOT NULL,
     qty INTEGER NOT NULL,
@@ -62,12 +62,12 @@ create table containss
     FOREIGN KEY (order_num) REFERENCES orders(order_num),
     FOREIGN KEY (sku) REFERENCES product(sku));
 
-create table sale 
-   (order_num integer NOT NULL,
+CREATE TABLE sale 
+   (order_num INTEGER NOT NULL,
     PRIMARY KEY (order_num),
     FOREIGN KEY (order_num) REFERENCES orders(order_num));
 
-create table supplier
+CREATE TABLE supplier
    (tin NUMERIC(9) NOT NULL,
     name VARCHAR(80) NOT NULL,
     address VARCHAR(30)	NOT NULL,
@@ -76,56 +76,56 @@ create table supplier
     PRIMARY KEY (tin),
     FOREIGN KEY (sku) REFERENCES product(sku));
 
-create table ean_product
+CREATE TABLE ean_product
    (sku VARCHAR(8) NOT NULL,
     ean VARCHAR(13) NOT NULL,
     PRIMARY KEY (sku),
     FOREIGN KEY (sku) REFERENCES product(sku));
 
-create table employee
-   (ssn numeric(9) NOT NULL,
-    tin numeric(9) NOT NULL unique,
-    UNIQUE(tin),
+CREATE TABLE employee
+   (ssn NUMERIC(9) NOT NULL,
+    tin NUMERIC(9) NOT NULL unique,
     bdate DATE NOT NULL,
     name VARCHAR(80) NOT NULL,
+    UNIQUE(tin),
     PRIMARY KEY (ssn));
     -- IC-3: Every employee (ssn) must participate in the works association --
 
-create table department
+CREATE TABLE department
    (name VARCHAR(80) NOT NULL,
     PRIMARY KEY (name));
 
-create table workplace
+CREATE TABLE workplace
    (address VARCHAR(30) NOT NULL,
     lat DECIMAL(8,6) NOT NULL,
-    long decimal(9,6) NOT NULL,
+    long DECIMAL(9,6) NOT NULL,
     UNIQUE (lat, long),
     PRIMARY KEY (address));
 
-create table office
+CREATE TABLE office
    (address VARCHAR(30) NOT NULL,
     PRIMARY KEY (address),
     FOREIGN KEY (address) REFERENCES workplace(address));
 
-create table warehouse
+CREATE TABLE warehouse
    (address VARCHAR(30) NOT NULL,
     PRIMARY KEY (address),
     FOREIGN KEY (address) REFERENCES workplace(address));
 
-create table pay
+CREATE TABLE pay
    (order_num INTEGER NOT NULL,
     customer_number INTEGER NOT NULL,
     PRIMARY KEY (order_num),
     FOREIGN KEY (order_num) REFERENCES sale(order_num),
     FOREIGN KEY (customer_number) REFERENCES customer(number));
 
-create table process
+CREATE TABLE process
    (order_num INTEGER NOT NULL,
     ssn NUMERIC(9) NOT NULL,
     PRIMARY KEY (order_num, ssn),
     FOREIGN KEY (order_num) REFERENCES orders(order_num));
 
-create table works
+CREATE TABLE works
    (ssn NUMERIC(9) NOT NULL,
     name VARCHAR(80) NOT NULL,
     address VARCHAR(30) NOT NULL,
@@ -134,7 +134,7 @@ create table works
     FOREIGN KEY (name) REFERENCES department(name),
     FOREIGN KEY (address) REFERENCES workplace(address));
 
-create table delivery
+CREATE TABLE delivery
    (address VARCHAR(30) NOT NULL,
     tin numeric(9) NOT NULL,
     PRIMARY KEY (address, tin),
