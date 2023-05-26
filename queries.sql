cb9d57cc0ec5a07f8de42324a0bf0f904911b52a
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2023-05-26 14:15:07 +0100

    ex2 de queries.sql concertado

--- queries.sql
+++ queries.sql
@@ -3,26 +3,30 @@ SELECT customer.name
 FROM customer
     NATURAL JOIN orders
     NATURAL JOIN containss
-    FULL OUTER JOIN product ON product.name = customer.name
-WHERE date > '2022-12-31' AND date < '2024-01-01' AND price > 50;
+    FULL OUTER JOIN product ON product.sku = containss.sku
+WHERE orders.date > '2022-12-31' AND orders.date < '2024-01-01' AND product.price > 50;
 
 ----- EXERCICIO 2 -----
 
-SELECT employee.name
+SELECT DISTINCT employee.name
 FROM employee
     NATURAL JOIN (
-        SELECT date
+        SELECT *
         FROM orders
-        WHERE date > '2022-31-12' AND date < '2023-02-01') AS january_requests
+        WHERE orders.date > '2022-12-31' AND orders.date < '2023-02-01'
+    ) as od
     NATURAL JOIN process
-    FULL OUTER JOIN works ON works.name = employee.name
-    NATURAL JOIN warehouse WHERE address NOT IN(
-        SELECT office.address FROM office);
+    FULL OUTER JOIN works ON works.ssn = employee.ssn
+    NATURAL JOIN warehouse
+    WHERE address NOT IN(
+        SELECT address
+        FROM office
+    );
 
 
 ----- EXERCICIO 3 -----
 SELECT name
-FROM sale NATURAL JOIN containss NATURAL JOIN product
+FROM product NATURAL JOIN containss NATURAL JOIN sale
 HAVING SUM(qty) >= ALL (
     SELECT SUM(qty)
     FROM containss
