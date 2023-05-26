58a4383394fc7b66aee855cc6642c3e5f642c1fb
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2023-05-26 21:52:42 +0100

    comentários

--- queries.sql
+++ queries.sql
@@ -1,4 +1,6 @@
------ EXERCICIO 1 -----
+-- EXERCICIO 1
+-- Πname(σdate>”31−12−2022” ∧ date<”1−1−2024” ∧ price>50 (customer ▷◁ order ▷◁ contains ▷◁ ρname→nameprod(product)))
+
 SELECT customer.name
 FROM customer
     JOIN orders o ON customer.number = o.customer_number
@@ -6,7 +8,9 @@ FROM customer
     JOIN product p ON contains.sku = p.sku
 WHERE p.price > 50 AND o.date > '2022-12-31' AND o.date < '2024-01-01';
 
------ EXERCICIO 2 -----
+-- EXERCICIO 2
+-- A ← σdate>”31−12−2022” ∧ date<”1−2−2023”(order ▷◁ process ▷◁ employee)
+-- Πname(A ▷◁ ρname→namework(works) ▷◁ (warehouse − office))
 
 SELECT DISTINCT employee.name
 FROM employee
@@ -20,7 +24,11 @@ WHERE address NOT IN(
 ) AND o.date > '2022-12-31' AND o.date < '2023-02-01';
 
 
------ EXERCICIO 3 -----
+-- EXERCICIO 3
+-- A ← ρ(2→qty)(skuGSU M (qty)(contains ▷◁ sale))
+-- B ← ρ(1→max)(GM AX(qty)(A))
+-- Πname(σqty=max(product ▷◁ A × B))
+
 SELECT name
 FROM product NATURAL JOIN contains NATURAL JOIN sale
 GROUP BY name
@@ -31,7 +39,9 @@ HAVING SUM(qty) >= ALL (
 )
 LIMIT 1;
 
------ EXERCICIO 4 -----
+-- EXERCICIO 4
+-- order_noGsum(total value)(Πorder_no,price×qty→total_value(sale ▷◁ contains ▷◁ product))
+
 SELECT order_num, SUM(product.price * contains.qty) AS total_value
 FROM sale NATURAL JOIN contains NATURAL JOIN product
 GROUP BY order_num
