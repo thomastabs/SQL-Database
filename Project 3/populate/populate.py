3a1edfcfaf51e3dc2aa2b83198af62355d4a7379
thomastabs <tomassantostaborda@gmail.com>
2023-06-03 18:34:17 +0100

    new populate, e cenas do Pycharm lmao

---
+++ Project 3/populate/populate.py
@@ -0,0 +1,32 @@
+def customers():
+    for line in open("customers.csv", "r"):
+        customer = line.strip().split(",")
+        ins = "INSERT INTO product VALUES ('{}', '{}', '{}', '{}', '{}');\n".format(customer[0], customer[1],
+                                                                                    customer[2], customer[3],
+                                                                                    customer[4])
+        file.write(ins)
+
+
+def products():
+    for line in open("products.csv", "r"):
+        product = line.strip().split(",")
+        # Se tem produto ean
+        if len(product) == 5:
+            ins = "INSERT INTO product VALUES ('{}', '{}', '{}', '{}', '{}');\n".format(product[0], product[1],
+                                                                                        product[2], product[3],
+                                                                                        product[4])
+            file.write(ins)
+        # Se tem produto normal
+        if len(product) == 4:
+            ins = "INSERT INTO product VALUES ('{}', '{}', '{}', '{}');\n".format(product[0], product[1],
+                                                                                  product[2], product[3])
+            file.write(ins)
+
+
+if __name__ == "__main__":
+    file = open('../populate/new_populate.sql', 'w')
+
+    products()
+    file.write('\n')
+
+    file.close()
