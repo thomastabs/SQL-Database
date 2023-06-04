c1dfd49fd335acf88eb67b280bcd92fe10020bba
thomastabs <tomassantostaborda@gmail.com>
2023-06-04 21:45:41 +0100

    populate part 2

--- Project 3/populate/populate.py
+++ Project 3/populate/populate.py
@@ -1,9 +1,8 @@
 def customers():
     for line in open("customers.csv", "r"):
         customer = line.strip().split(",")
-        ins = "INSERT INTO customer VALUES ('{}', '{}', '{}', '{}', '{}');\n".format(customer[0], customer[1],
-                                                                                    customer[2], customer[3],
-                                                                                    customer[4])
+        ins = "INSERT INTO customer VALUES ('{}', '{}', '{}', '{}');\n".format(customer[0], customer[1],
+                                                                                    customer[2], customer[3])
         file.write(ins)
 
 
@@ -23,10 +22,32 @@ def products():
             file.write(ins)
 
 
+def orders():
+    for line in open("orders.csv", "r"):
+        order = line.strip().split(",")
+        ins = "INSERT INTO orders VALUES ('{}', '{}', '{}');\n".format(order[0], order[1], order[2])
+        file.write(ins)
+
+
+def employees():
+    for line in open("employees.csv", "r"):
+        employee = line.strip().split(",")
+        ins = "INSERT INTO employee VALUES ('{}', '{}', '{}', '{}');\n".format(employee[0], employee[1],
+                                                                               employee[2], employee[3])
+        file.write(ins)
+
+
 if __name__ == "__main__":
-    file = open('../populate/new_populate.sql', 'w')
+    file = open('../populate/populate.sql', 'w')
 
     products()
     file.write('\n')
+    customers()
+    file.write('\n')
+    orders()
+    file.write('\n')
+    employees()
+    file.write('\n')
+
 
     file.close()
--- Project 3/populate/populate.sql
+++ Project 3/populate/populate.sql
@@ -1,106 +1,49 @@
-DROP TABLE IF EXISTS customer CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS pay CASCADE;
-DROP TABLE IF EXISTS employee CASCADE;
-DROP TABLE IF EXISTS process CASCADE;
-DROP TABLE IF EXISTS department CASCADE;
-DROP TABLE IF EXISTS workplace CASCADE;
-DROP TABLE IF EXISTS works CASCADE;
-DROP TABLE IF EXISTS office CASCADE;
-DROP TABLE IF EXISTS warehouse CASCADE;
-DROP TABLE IF EXISTS product CASCADE;
-DROP TABLE IF EXISTS contains CASCADE;
-DROP TABLE IF EXISTS supplier CASCADE;
-DROP TABLE IF EXISTS delivery CASCADE;
+INSERT INTO product VALUES ('10000001', 'Vassoura', 'Vassoura de qualidade industrial', '4.20', '345678');
+INSERT INTO product VALUES ('10000002', 'Detergente', 'Detergente multi usos tão bom que o Fairy até chora', '9.60');
+INSERT INTO product VALUES ('10000003', 'Walkman', ' Sony Walkman - dispositivo de audição de música em cassete de alta qualidade', '300.00', '12345');
+INSERT INTO product VALUES ('10000004', 'Batatas', 'Saco de batatas irlandesas recem colhidas', '6.90');
+INSERT INTO product VALUES ('10000005', 'PS5', 'Consola Playstation de nova geração', '500', ' 565656');
+INSERT INTO product VALUES ('10000006', 'Geforce RTX 3060', 'Placa Gráfica Geforce RTX da NVIDIA', '300');
+INSERT INTO product VALUES ('10000007', 'Cooler', 'Sistema de Arrefecimento para um computador', '70', ' 232323');
+INSERT INTO product VALUES ('10000008', 'Fonte de Alimentação', 'Fonte de Energia para um computador', '90');
+INSERT INTO product VALUES ('10000009', 'SSD de 500gb', 'Armazenamento SSD de tamanho 500gb', '120', ' 1212');
+INSERT INTO product VALUES ('10000010', '12Gb de RAM', '2 cartões de RAM que juntos fazem 12 GB', '200');
 
-CREATE TABLE customer (
-    cust_no INTEGER PRIMARY KEY,
-    name VARCHAR(80) NOT NULL,
-    email VARCHAR(254) UNIQUE NOT NULL,
-    phone VARCHAR(15),
-    address VARCHAR(255)
-);
+INSERT INTO customer VALUES ('1', 'Adams', 'adams12345@gmail.com', '918138586');
+INSERT INTO customer VALUES ('2', 'John', 'johnxina@gmail.com', '918134543');
+INSERT INTO customer VALUES ('3', 'Bob', 'bob12345@gmail.com', '923423452');
+INSERT INTO customer VALUES ('4', 'Steve', 'steve12345@gmail.com', '917123411');
+INSERT INTO customer VALUES ('5', 'Leon', 'leon1234@gmail.com', '919519439');
+INSERT INTO customer VALUES ('6', 'Ashley', 'ashley1234@gmail.com', '918410660');
+INSERT INTO customer VALUES ('7.Thomas', 'thomas1234@gmail.com', '921324777', 'Rua Helena Tavares 2725-059 Mem Martins');
+INSERT INTO customer VALUES ('8', 'Will', 'will1234@gmail.com', '911222111');
+INSERT INTO customer VALUES ('9', 'Tyler', 'tyler1234@gmail.com', '920988756');
+INSERT INTO customer VALUES ('10', 'Drake', 'drake1234@gmail.com', '965888232');
 
-CREATE TABLE orders (
-    order_no INTEGER PRIMARY KEY,
-    cust_no INTEGER NOT NULL REFERENCES customer,
-    date DATE NOT NULL
-    --order_no must exist in contains
-);
+INSERT INTO orders VALUES ('1', '1', '2023-01-04');
+INSERT INTO orders VALUES ('2', '2', '2023-02-10');
+INSERT INTO orders VALUES ('3', '1', '2023-01-17');
+INSERT INTO orders VALUES ('4', '3', '2023-01-23');
+INSERT INTO orders VALUES ('5', '4', '2023-05-11');
+INSERT INTO orders VALUES ('6', '5', '2023-09-02');
+INSERT INTO orders VALUES ('7', '6', '2023-11-29');
+INSERT INTO orders VALUES ('8', '8', '2023-04-01');
+INSERT INTO orders VALUES ('9', '5', '2023-03-05');
+INSERT INTO orders VALUES ('10', '7', '2023-08-16');
+INSERT INTO orders VALUES ('11', '7', '2023-01-21');
+INSERT INTO orders VALUES ('12', '9', '2023-05-04');
+INSERT INTO orders VALUES ('13', '10', '2023-07-02');
+INSERT INTO orders VALUES ('14', '2', '2023-11-13');
+INSERT INTO orders VALUES ('15', '1', '2023-09-19');
 
-CREATE TABLE pay(
-    order_no INTEGER PRIMARY KEY REFERENCES orders,
-    cust_no INTEGER NOT NULL REFERENCES customer
-);
-
-CREATE TABLE employee(
-    ssn VARCHAR(20) PRIMARY KEY,
-    TIN VARCHAR(20) UNIQUE NOT NULL,
-    bdate DATE,
-    name VARCHAR NOT NULL
-    --age must be >=18
-);
-
-CREATE TABLE process(
-    ssn VARCHAR(20) REFERENCES employee,
-    order_no INTEGER REFERENCES orders,
-    PRIMARY KEY (ssn, order_no)
-);
-
-CREATE TABLE department(
-    name VARCHAR PRIMARY KEY
-);
-
-CREATE TABLE workplace(
-    address VARCHAR PRIMARY KEY,
-    lat NUMERIC(8, 6) NOT NULL,
-    long NUMERIC(9, 6) NOT NULL,
-    UNIQUE(lat, long)
-    --address must be either in warehouse or office
-    --address cannot be both in warehouse and office simultaneously
-);
-
-CREATE TABLE office(
-    address VARCHAR(255) PRIMARY KEY REFERENCES workplace
-);
-
-CREATE TABLE warehouse(
-    address VARCHAR(255) PRIMARY KEY REFERENCES workplace
-);
-
-CREATE TABLE works(
-    ssn VARCHAR(20) REFERENCES employee,
-    name VARCHAR(200) REFERENCES department,
-    address VARCHAR(255) REFERENCES workplace,
-    PRIMARY KEY (ssn, name, address)
-);
-
-CREATE TABLE product(
-    SKU VARCHAR(25) PRIMARY KEY,
-    name VARCHAR(200) NOT NULL,
-    description VARCHAR,
-    price NUMERIC(10, 2) NOT NULL,
-    ean NUMERIC(13) UNIQUE
-);
-
-CREATE TABLE contains(
-    order_no INTEGER REFERENCES orders,
-    SKU VARCHAR(25) REFERENCES product,
-    qty INTEGER,
-    PRIMARY KEY (order_no, SKU)
-);
-
-CREATE TABLE supplier(
-    TIN VARCHAR(20) PRIMARY KEY,
-    name VARCHAR(200),
-    address VARCHAR(255),
-    SKU VARCHAR(25) REFERENCES product,
-    date DATE
-);
-
-CREATE TABLE delivery(
-    address VARCHAR(255) REFERENCES warehouse,
-    TIN VARCHAR(20) REFERENCES supplier,
-    PRIMARY KEY (address, TIN)
-);
+INSERT INTO employee VALUES ('111111111', '123456789', '1990-01-01', 'Tomás');
+INSERT INTO employee VALUES ('222222222', '987654321', '1995-02-02', 'Mateus');
+INSERT INTO employee VALUES ('333333333', '123112323', '1999-04-29', 'Rodrigo');
+INSERT INTO employee VALUES ('444444444', '321999872', '1989-01-29', 'Maia');
+INSERT INTO employee VALUES ('555555555', '233333444', '1991-08-14', 'Fábio');
+INSERT INTO employee VALUES ('666666666', '555554444', '1992-12-03', 'José');
+INSERT INTO employee VALUES ('777777777', '666666664', '1993-10-08', 'Nelson');
+INSERT INTO employee VALUES ('888888888', '232324444', '1994-06-09', 'Isabel');
+INSERT INTO employee VALUES ('999999999', '111111144', '1995-07-12', 'Gonçalo');
+INSERT INTO employee VALUES ('100000000', '555444777', '1996-09-21', 'João');
 
