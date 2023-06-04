d87a1eaf361aec50a4a9c8729463d8698118f24b
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2023-06-04 15:38:13 +0100

    queries acho que ja estão certas

--- Project 3/queries.sql
+++ Project 3/queries.sql
@@ -1,20 +1,23 @@
 ------- QUERY 1 -------
-SELECT customer_number, customer.name
-FROM customer NATURAL JOIN pay NATURAL JOIN contains
-    JOIN product p on p.sku = contains.sku
-GROUP BY customer_number, customer.name
-HAVING SUM(price * qty) >= ALL (
-    SELECT SUM(price * qty)
-    FROM pay NATURAL JOIN product NATURAL JOIN contains
-    GROUP BY customer_number
-);
+SELECT c.number, c.name
+FROM customer c JOIN pay p ON c.number = p.customer_number
+GROUP BY c.number, c.name
+HAVING COUNT(p.order_num) >= ALL (
+    SELECT COUNT(p.order_num) FROM pay p
+    GROUP BY p.customer_number
+)
+ORDER BY c.number;
 
 ------- QUERY 2 -------
-(SELECT name, date FROM employee
- JOIN process p on employee.ssn = p.ssn
- JOIN orders o on o.order_num = p.order_num)
-EXCEPT
-(SELECT date FROM orders WHERE EXTRACT(YEAR from date) == 2022);
+SELECT e.name FROM employee e
+WHERE NOT EXISTS (
+    SELECT DISTINCT o.date FROM orders o
+    WHERE EXTRACT(YEAR FROM o.date) = 2022
+    AND NOT EXISTS (
+        SELECT p.order_num FROM process p
+        WHERE p.ssn = e.ssn AND p.order_num = o.order_num
+    )
+);
 
 ------- QUERY 3 -------
 SELECT EXTRACT(MONTH FROM o.date) AS month, COUNT(*)
@@ -22,7 +25,6 @@ FROM orders o
 WHERE o.order_num NOT IN(
     SELECT order_num
     FROM pay
-    )
-AND o.date >= '2022-01-01' AND o.date < '2023-01-01'
+) AND o.date >= '2022-01-01' AND o.date < '2023-01-01'
 GROUP BY month
 ORDER BY month;
