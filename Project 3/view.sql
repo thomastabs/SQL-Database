6789036e4794fc64c316e9d25083ef79ba5dbe73
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2023-06-07 12:05:30 +0100

    view acho que ja esta bacana, o teste e para os indices

--- Project 3/view.sql
+++ Project 3/view.sql
@@ -9,7 +9,7 @@ SELECT c.SKU,
        EXTRACT(MONTH FROM o.date) AS month,
        EXTRACT(DAY FROM o.date) AS day_of_month,
        EXTRACT(DOW FROM o.date) AS day_of_week,
-       REGEXP_MATCHES(address, '(\d+) ([[:alpha:]]+)', 'g') AS city
+       TRIM(REGEXP_REPLACE(cu.address, '.*([0-9]{4}-[0-9]{3}) ([[:alpha:] ]+)$', '\2')) AS city
 FROM contains c
 JOIN orders o ON c.order_no = o.order_no
 JOIN pay py ON o.order_no = py.order_no
