764b359559cec974e22ec6dd49fc1394673d921d
thomastabs <tomassantostaborda@gmail.com>
2023-06-11 19:09:51 +0100

    we ball

--- Project 3/web/main.py
+++ Project 3/web/main.py
@@ -4,112 +4,210 @@ import cgi
 
 
 def pay_an_order(form, cursor, connection):
-    order_id = form.getvalue('order_id')
+    order_id = form.getvalue('pay_order_no')
     customer_num = form.getvalue('customer_no')
 
+    # Making SQL query to verify if an order with that order number exists
+    cursor.execute("SELECT * FROM orders WHERE order_no = %(order_no)s", {'order_no': order_id})
+    order = cursor.fetchone()
+
+    if order is None:
+        print("<h1>Error: Order with this ID does not exist</h1>")
+        print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
+        return
+
+    # Making SQL query to verify if a customer with that customer number exists
+    cursor.execute("SELECT * FROM customer WHERE cust_no = %(customer_number)s", {'customer_number': customer_num})
+    customer = cursor.fetchone()
+
+    if customer is None:
+        print("<h1>Error: Customer not found</h1>")
+        print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
+        return
+
     # Making SQL Query
-    sql = "INSERT INTO pay VALUES(%(order_number), %(customer_number))", {'order_number': order_id,
-                                                                          'customer_number': customer_num}
-    cursor.execute(sql)
+    cursor.execute("INSERT INTO pay VALUES(%(order_number), %(customer_number))",
+                   {'order_number': order_id,
+                    'customer_number': customer_num})
+
     connection.commit()
     print("<h1>Order paid successfully</h1>")
-
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 def modify_product_price(form, cursor, connection):
     product_id = form.getvalue('product_id_price')
     new_price = form.getvalue('new_price')
 
     # Making SQL query to find a product and update its price
-    sql = "SELECT * FROM product WHERE SKU = %(product_id)s", {'product_id': product_id}
-    cursor.execute(sql)
+    cursor.execute("SELECT * FROM product WHERE SKU = %(product_id)s", {'product_id': product_id})
     product = cursor.fetchone()
 
     if product is None:
         print("<h1>Error: Product not found</h1>")
         print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
         return
 
-    sql = "UPDATE product SET price = %(price)s WHERE SKU = %(product_id)s", {'price': new_price,
-                                                                              'product_id': product_id}
     # Update the product price
-    cursor.execute(sql)
+    cursor.execute("UPDATE product SET price = %(price)s WHERE SKU = %(product_id)s",
+                   {'price': new_price,
+                    'product_id': product_id})
+
     connection.commit()
-
     print("<h1>Product price updated successfully</h1>")
-
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 def modify_product_description(form, cursor, connection):
     product_id = form.getvalue('product_id_description')
     new_descr = form.getvalue('new_description')
 
     # Making SQL query to find a product and update its description
-    sql = "SELECT * FROM product WHERE SKU = %(product_id)s", {'product_id': product_id}
-    cursor.execute(sql)
+    cursor.execute("SELECT * FROM product WHERE SKU = %(product_id)s", {'product_id': product_id})
     product = cursor.fetchone()
 
     if product is None:
         print("<h1>Error: Product not found</h1>")
         print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
         return
 
-    sql = "UPDATE product SET description = %(description)s WHERE SKU = %(product_id)s", {'description': new_descr,
-                                                                                          'product_id': product_id}
     # Update the product price
-    cursor.execute(sql)
-    connection.commit()
+    cursor.execute("UPDATE product SET description = %(description)s WHERE SKU = %(product_id)s",
+                   {'description': new_descr,
+                    'product_id': product_id})
 
+    connection.commit()
     print("<h1>Product description updated successfully</h1>")
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 
 def remove_product(form, cursor, connection):
-    product_id = form.getvalue('product_id')
+    product_id = form.getvalue('product_remove_sku')
 
     # Making SQL query to find a product and delete it
-    sql = "SELECT * FROM product WHERE SKU = %(product_id)s", {'product_id': product_id}
-    cursor.execute(sql)
+    cursor.execute("SELECT * FROM product WHERE SKU = %(product_id)s", {'product_id': product_id})
     product = cursor.fetchone()
 
     if product is None:
         print("<h1>Error: Product not found</h1>")
         print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
         return
 
-    sql = "DELETE FROM product WHERE SKU = %(product_id)s", {'product_id': product_id}
     # Delete the product
-    cursor.execute(sql)
+    cursor.execute("DELETE FROM product WHERE SKU = %(product_id)s", {'product_id': product_id})
     connection.commit()
 
     print("<h1>Product deleted successfully</h1>")
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 
 def remove_customer(form, cursor, connection):
-    customer_id = form.getvalue('customer_id')
+    customer_id = form.getvalue('customer_remove_id')
 
-    # Making SQL query to find a product and delete it
-    sql = "SELECT * FROM customer WHERE cust_no = %(customer_number)s", {'customer_number': customer_id}
-    cursor.execute(sql)
+    # Making SQL query to find a customer and delete it
+    cursor.execute("SELECT * FROM customer WHERE cust_no = %(customer_number)s", {'customer_number': customer_id})
     customer = cursor.fetchone()
 
     if customer is None:
         print("<h1>Error: Customer not found</h1>")
         print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
         return
 
-    sql = "DELETE FROM customer WHERE cust_no = %(customer_number)s", {'customer_number': customer_id}
     # Delete the customer
-    cursor.execute(sql)
+    cursor.execute("DELETE FROM customer WHERE cust_no = %(customer_number)s", {'customer_number': customer_id})
     connection.commit()
 
     print("<h1>Customer deleted successfully</h1>")
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 
 def make_an_order(form, cursor, connection):
     # Question 1: Should I also make a product_sku and qty field form for putting the order num in contain?
-    return form
+    order_id = form.getvalue('make_order_no')
+    customer_id = form.getvalue('client_id_order')
+    date = form.getvalue('date')
+    product_id = form.getvalue('product_order_sku')
+    qty = form.getvalue('qty')
 
+    # Making SQL query to verify if an order with that order number exists
+    cursor.execute("SELECT * FROM orders WHERE order_no = %(order_no)s", {'order_no': order_id})
+    order = cursor.fetchone()
+
+    if order is not None:
+        print("<h1>Error: Order with this ID already exists</h1>")
+        print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
+        return
+
+    # Making SQL query to verify if the product exists
+    cursor.execute("SELECT * FROM product WHERE SKU = %(product_id)s", {'product_id': product_id})
+    product = cursor.fetchone()
+
+    if product is None:
+        print("<h1>Error: Product not found</h1>")
+        print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
+        return
+
+    # Making SQL query to verify if the customer exists
+    cursor.execute("SELECT * FROM customer WHERE cust_no = %(customer_number)s", {'customer_number': customer_id})
+    customer = cursor.fetchone()
+
+    if customer is None:
+        print("<h1>Error: Customer not found</h1>")
+        print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
+        return
+
+    connection.commit()
+
+
+    # Now we can finally make an order, with the transaction method
+    cursor.execute("INSERT INTO orders VALUES(%(order_no)s, %(cust_no)s, %(date)s)",
+                   {'order_no': order_id, 'cust_no': customer_id, 'date': date})
+    cursor.execute("INSERT INTO contains VALUES(%(order_no)s, %(product_sku)s, %(quantity)s",
+                   {'order_no': order_id, 'product_sku': product_id, 'quantity': qty})
+
+    connection.commit()
+    print("<h1>Order made successfully</h1>")
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 def register_product(form, cursor, connection):
-    product_sku = form.getvalue('product_sku')
+    product_sku = form.getvalue('product_register_sku')
     product_name = form.getvalue('product_name')
     product_descr = form.getvalue('product_description')
     product_price = form.getvalue('product_price')
@@ -119,25 +217,33 @@ def register_product(form, cursor, connection):
     elif not product_ean.isdigit():
         print("<h1>Product EAN is not a numeric sequence</h1>")
         print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
         return
 
     # Making SQL Query
-    sql = "INSERT INTO product VALUES(%(product_sku), %(product_name), %(product_description), %(product_price), " \
-          "%(product_ean))", {'product_sku': product_sku, 'product_name': product_name,
-                              'product_description': product_descr, 'product_price': product_price,
-                              'product_ean': product_ean}
+    cursor.execute("INSERT INTO product VALUES(%(product_sku)s, %(product_name)s, %(product_description)s,%(product_price)s, %(product_ean)s)",
+                   {'product_sku': product_sku, 'product_name': product_name,
+                    'product_description': product_descr, 'product_price': product_price,
+                    'product_ean': product_ean})
 
-    cursor.execute(sql)
     connection.commit()
     print("<h1>Product registed successfully</h1>")
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 
 def register_customer(form, cursor, connection):
     customer_id = form.getvalue('customer_register_id')
     customer_name = form.getvalue('customer_register_name')
-    if customer_name is not str:
+    if not isinstance(customer_name, str):
         print("<h1>Customer Name should be only letters</h1>")
         print('<p>Error found, terminating operation</p>')
+        print(" <form action='index.HTML'>")
+        print("     <input type='submit' value='Go Back'>")
+        print(" </form>")
         return
     # Idk how to verify this
     customer_email = form.getvalue('customer_register_email')
@@ -145,16 +251,17 @@ def register_customer(form, cursor, connection):
     # Idk how to verify this 100% correctly
     customer_address = form.getvalue('customer_register_address')
 
-
     # Making SQL Query
-    sql = "INSERT INTO customer VALUES(%(customer_id), %(customer_name), %(customer_email), %(customer_phone), " \
-          "%(customer_address))", {'customer_id': customer_id, 'customer_name': customer_name,
-                                   'customer_email': customer_email, 'customer_phone': customer_phone,
-                                   'customer_address': customer_address}
+    cursor.execute("INSERT INTO customer VALUES(%(customer_id)s, %(customer_name)s, %(customer_email)s, %(customer_phone)s, %(customer_address)s)",
+                   {'customer_id': customer_id, 'customer_name': customer_name,
+                    'customer_email': customer_email, 'customer_phone': customer_phone,
+                    'customer_address': customer_address})
 
-    cursor.execute(sql)
     connection.commit()
     print("<h1>Customer registed successfully</h1>")
+    print(" <form action='index.HTML'>")
+    print("     <input type='submit' value='Go Back'>")
+    print(" </form>")
 
 
 ist_id = 'ist1103641'
@@ -185,31 +292,35 @@ try:
     cursor = connection.cursor()
 
     form = cgi.FieldStorage()
-    if len(form) == 1:
-        # If the length is 1, then the forms are divided between removing products or removing a customer
-        if any(form[field].value == 'product_id' for field in form):
-            remove_product(form, cursor, connection)
-        else:
-            remove_customer(form, cursor, connection)
-    elif len(form) == 2:
-        # If the length is 2, then the forms are divided between
-        # modifying product prices or descriptions or paying an order
-        if any(form[field].value == 'order_id' for field in form):
-            pay_an_order(form, cursor, connection)
-        elif any(form[field].value == 'product_id_price' for field in form):
-            modify_product_price(form, cursor, connection)
-        else:
-            modify_product_description(form, cursor, connection)
-    elif len(form) == 4:
-        make_an_order(form, cursor, connection)
-    else:
-        # If the length is bigger than 4, then the forms can be
-        # only register a product or register a customer
-        if any(form[field].value == 'product_sku' for field in form):
-            register_product(form, cursor, connection)
-        else:
-            register_customer(form, cursor, connection)
 
+    # Help for debug
+    print(len(form))
+    for field in form.keys():
+        value = form.getvalue(field)
+        print(f"Field: {field}")
+        print(f"Value: {value}")
+        print('\n')
+
+    if any(field == 'product_register_sku' for field in form.keys()):
+        register_product(form, cursor, connection)
+    elif any(field == 'product_remove_sku' for field in form.keys()):
+        remove_product(form, cursor, connection)
+    elif any(field == 'product_id_price' for field in form.keys()):
+        modify_product_price(form, cursor, connection)
+    elif any(field == 'product_id_description' for field in form.keys()):
+        modify_product_description(form, cursor, connection)
+    elif any(field == 'customer_register_id' for field in form.keys()):
+        register_customer(form, cursor, connection)
+    elif any(field == 'customer_remove_id' for field in form.keys()):
+        remove_customer(form, cursor, connection)
+    elif any(field == 'make_order_no' for field in form.keys()):
+        make_an_order(form, cursor, connection)
+    elif any(field == 'pay_order_no' for field in form.keys()):
+        pay_an_order(form, cursor, connection)
+    else:
+        print('<p>Unknown Operation</p>')
+
+    print('<p>Operation finished</p>')
     cursor.close()
     connection.close()
 
