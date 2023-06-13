06c5099b8a577b408760e3e24e97f18712595c03
Fábio Sobrinho <fabiosobrinho350@gmail.com>
2023-06-13 20:16:27 +0100

    esta query devia estar a funcionar

--- Project 3/queries.sql
+++ Project 3/queries.sql
@@ -10,10 +10,24 @@ HAVING SUM(price * qty) >= ALL (
     GROUP BY cust_no
 );
 
+
+
+------- QUERY 2 -------
+SELECT DISTINCT name FROM (employee e
+    JOIN process p ON p.ssn = e.ssn
+    NATURAL JOIN orders)
+WHERE EXTRACT(YEAR FROM date) = 2022
+GROUP BY name
+HAVING COUNT(DISTINCT date) = (
+  SELECT COUNT(DISTINCT date)
+  FROM orders NATURAL JOIN process
+  WHERE EXTRACT(YEAR FROM date) = 2022
+);
+
 ------- QUERY 2 -------
 SELECT DISTINCT e.name FROM employee e
 WHERE EXISTS (
-    SELECT DISTINCT o.date FROM orders o
+    SELECT DISTINCT o.date FROM orders o NATURAL JOIN process
     WHERE EXTRACT(YEAR FROM o.date) = 2022
     AND EXISTS (
         SELECT p.order_no FROM process p
